148eaf664619b5b23ca29211ce9f7f755121ff4d
rybaaa <bolshayaryba22@gmail.com>
2023-02-18 00:38:08 +0300

    fix: links to gitHub repo

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,12 +1,18 @@
-import React from 'react';
 import s from './Projects.module.scss'
 import {Project} from "./project/Project";
 import {Title} from "../common/components/Title/Title";
 import todoImg from '../assets/img/todolist.jpg'
 import socNetImg from '../assets/img/social-network.jpg'
 import flashcardsImg from '../assets/img/flashcards.jpeg'
+import reposImg from '../assets/img/gitgraph.png'
 import Fade from 'react-reveal/Fade';
-
+import React from "react";
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/scss";
+import "swiper/scss/effect-coverflow";
+import "swiper/scss/pagination";
+import '../projects/slider.css'
+import {Pagination, EffectCoverflow} from "swiper";
 
 
 export const Projects = () => {
@@ -19,16 +25,55 @@ export const Projects = () => {
     const flashcards = {
         backgroundImage: `url(${flashcardsImg})`
     }
+    const repos = {
+        backgroundImage: `url(${reposImg})`
+    }
     return (
         <div id='projects' className={s.wrapper}>
             <div className={s.projectsBlock}>
-                <Title title={'Projects'} text = {'VISIT MY GITHUB AND KEEP YOUR FEEDBACK'}/>
+                <Title title={'Projects'} text={'VISIT MY GITHUB AND KEEP YOUR FEEDBACK'}/>
                 <Fade left>
                     <div className={s.allProjectsBlock}>
-                        <Project style = {flashcards} title = {'FlashCards'} link={'https://github.com/rybaaa/flashcards'} description={'Open-source start-up project using React-hooks, Redux Toolkit, Axios and Rest API'}/>
-                        <Project style = {todo} title = {'Todolist'} link={'https://github.com/rybaaa/React-Todolist'} description={'Project for your daily and work duties using React-hooks, Redux, Axios and Rest API'}/>
-                        <Project style = {socNet} title = {'Social Network'} link={'https://github.com/rybaaa/social-network'}  description={'Developers social network. Many features still in progress. But you can use base functions'}/>
+                        <Project style={flashcards} title={'FlashCards'} link={'https://github.com/rybaaa/flashcards'}
+                                 description={'Open-source start-up project using React-hooks, Redux Toolkit, Axios and Rest API'}/>
+                        <Project style={repos} title={'Searching of GitHub repositories'}
+                                 link={'https://github.com/rybaaa/Repos-with-GitHub-GraphQL'}
+                                 description={'Project using JavaScript, Vue, Vuex, GraphQL, Axios'}/>
+                        <Project style={todo} title={'Todolist'} link={'https://github.com/rybaaa/React-Todolist'}
+                                 description={'Project for your daily and work duties using React-hooks, Redux, Axios and Rest API'}/>
+                        <Project style={socNet} title={'Social Network'}
+                                 link={'https://github.com/rybaaa/social-network'}
+                                 description={'Developers social network. Many features still in progress. But you can use base functions'}/>
                     </div>
+                    <Swiper
+                        effect={"coverflow"}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        slidesPerView={"auto"}
+                        coverflowEffect={{
+                            rotate: 50,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 1,
+                            slideShadows: true,
+                        }}
+                        pagination={true}
+                        modules={[EffectCoverflow, Pagination]}
+                        className={s.mySwiper}
+                    >
+                        <SwiperSlide> <Project style={flashcards} title={'FlashCards'}
+                                               link={'https://github.com/rybaaa/flashcards'}
+                                               description={'Open-source start-up project using React-hooks, Redux Toolkit, Axios and Rest API'}/>
+                        </SwiperSlide>
+                        <SwiperSlide><Project style={repos} title={'Searching of GitHub repositories'}
+                                              link={'https://github.com/rybaaa/Repos-with-GitHub-GraphQL'}
+                                              description={'Project using JavaScript, Vue, Vuex, GraphQL, Axios'}/></SwiperSlide>
+                        <SwiperSlide><Project style={todo} title={'Todolist'} link={'https://github.com/rybaaa/React-Todolist'}
+                                              description={'Project for your daily and work duties using React-hooks, Redux, Axios and Rest API'}/></SwiperSlide>
+                        <SwiperSlide><Project style={socNet} title={'Social Network'}
+                                              link={'https://github.com/rybaaa/social-network'}
+                                              description={'Developers social network. Many features still in progress. But you can use base functions'}/></SwiperSlide>
+                    </Swiper>
                 </Fade>
             </div>
         </div>
